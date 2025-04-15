050e7cd5b56cf1dad724a5e4eccf831d88b7ad2a
Camilla Barros <camillaerbarros@gmail.com>
2025-04-15 18:26:11 -0300

    cria arquivo de exercícios da semana de ooperadores, tipos e variáveis

---
+++ operadores-tipos-variaveis/exercícios.ts
@@ -0,0 +1,24 @@
+// exercício 1 - nível 1
+let aresta: number = 2;
+let pTrianguloEquilatero: number = aresta * 3;
+console.log(pTrianguloEquilatero);
+
+// exerciocio 2 - nível 1
+aresta = 3;
+let pQuadrado: number = aresta * 4;
+console.log(pQuadrado);
+
+// exercício 3 - nível 1
+let base: number = 5;
+let altura: number = 3;
+let pRetangulo: number = base * altura;
+console.log(pRetangulo);
+
+// exercicio 4 - nível 1
+let lado1: number = 2;
+let lado2: number = 3;
+let lado3: number = 6;
+let lado4: number = 8;
+let lado5: number = 10;
+let pPentagono: number = lado1 + lado2 + lado3 + lado4 + lado5;
+console.log(pPentagono);
