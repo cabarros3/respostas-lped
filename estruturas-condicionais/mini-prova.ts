027608dc237c14a2ccac82c81896c82ab262e4cc
Camilla Barros <camillaerbarros@gmail.com>
2025-05-11 14:50:26 -0300

    atualiza arquivos

--- estruturas-condicionais/mini-prova.ts
+++ estruturas-condicionais/mini-prova.ts
@@ -1,25 +1,48 @@
 // questão 1
 let salario: number = 1518;
-let rendaPerCapta: number = salario / 2;
+let rendaPerCapta: number = salario / 2; // 759
 let escolaPublica: boolean = true;
 let tipo: string = "";
 
+// = recebe
+// == igual
+// === igual em tipo e conteúdo
+
+// template string
+
 console.log(salario);
-console.log(rendaPerCapta);
+// console.log(rendaPerCapta);
 
-if (rendaPerCapta < 800 && escolaPublica == true) {
-  tipo = "Cota por renda";
-  console.log(`Aluno contemplado com ${tipo}`);
-} else if (rendaPerCapta >= 800 && escolaPublica == true) {
-  tipo = "Cota por escola pública";
-  console.log(`Aluno contemplado com ${tipo}`);
-} else {
-  console.log(
-    "Aluno não foi contemplado com nenhuma cota e concorrerá em ampla concorrência"
-  );
-}
+// if (rendaPerCapta <= 759 && escolaPublica == true) {
+//   tipo = "Cota por renda";
+//   console.log(`Aluno contemplado com ${tipo}`);
+//   // "aluno comtemplado" + tipo
+// } else if (rendaPerCapta > 759 && escolaPublica == true) {
+//   tipo = "Cota por escola pública";
+//   console.log(`Aluno contemplado com ${tipo}`);
+// } else {
+//   console.log(
+//     "Aluno não foi contemplado com nenhuma cota e concorrerá em ampla concorrência"
+//   );
+// }
 
-// questão 2
+//ou
+
+// if (salario < salario * 1.5) {
+//   if (escolaPublica == true) {
+//     console.log("cota por renda");
+//   }
+// } else if (salario >= salario * 1.5) {
+//   if (escolaPublica == true) {
+//     console.log("cota por escola pública");
+//   }
+// } else {
+//   console.log("ampla concorrência");
+// }
+
+// console.log(salario * 1.5);
+
+//questão 2
 let s: string = "i";
 let t: string = "r";
 let r: string = "e";
---
+++ operadores-logicos/exericios-extras.ts
@@ -0,0 +1,51 @@
+// se há pelo menos 1 número par
+
+// let a: number = 9;
+// let b: number = 3;
+// let c: number = 5;
+
+// if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+//   console.log("Pelo menos 1 número é par");
+// } else {
+//   console.log("Não há nenhum número par");
+// }
+
+// impar e positivo
+// let d: number = 3;
+
+// if (d >= 0 && d % 2 != 0) {
+//   console.log("é positivo e ímpar");
+// } else {
+//   console.log("Não é positivo e ímpar");
+// }
+
+// dois números pares
+
+// let num1: number = 2;
+// let num2: number = 3;
+// let num3: number = 4;
+// let num4: number = 5;
+
+// let contaImpares: number = 0;
+
+
+
+// verificar se há pelo menos 1 número par
+
+let a: number = 3
+let b: number = 2
+let c: number = 5
+let d: number = 2
+
+let pares: number = 0
+
+
+
+// oq é uma variavel
+// como pode nomear uma var
+// como a gente pode tipar uma variável
+// quais operações matematicas (+-/*)
+// quais operações logicas
+// estrutura condicional
+// laço de repetição
+// função
