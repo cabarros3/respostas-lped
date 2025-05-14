4e09beea82c4946615f8a75b0d8aaadaab353440
Camilla Barros <camillaerbarros@gmail.com>
2025-05-14 18:52:42 -0300

    adiciona comentários ao arquivo da miniprova de 2024

--- lacos-repeticao/miniprova-2024-tsi.ts
+++ lacos-repeticao/miniprova-2024-tsi.ts
@@ -9,13 +9,21 @@ while (contador < 3) {
 
 // ou
 
-let count: number = 0;
-let numero: number = 3;
-let incremento: number = 3;
+/**
+ * na questão ele dá a sequência: 3, 6, 11, 18, 27, 38, 51, 66
+ * mas como formar essa sequência?
+ * aqui usamos uma vriável número para receber o número da sequência da vez
+ * e para pular para o próximo número da sequencia acrescenta ao número um incremento
+ *
+ */
+
+let count: number = 0; // contador
+let numero: number = 3; // primeiro número da sequencia
+let incremento: number = 3; // o primeiro incremento
 
 while (count < 3) {
-  console.log(numero);
-  count++;
+  console.log(numero); // imprime o número da vez
+  count++; // incrementa o count
   incremento += 2; // próximo incremento aumenta em 2
-  numero += incremento;
+  numero += incremento; // número recebe o número da vez + o incremento (2)
 }
